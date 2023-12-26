132730c9f0d6be6664045beed5610525bbeccf6e
wzliu <80890392+WuTi0525@users.noreply.github.com>
2023-12-26 10:54:13 +0800

    Add files via upload

---
+++ 两数之和.cpp
@@ -0,0 +1,43 @@
+//2023.12.26
+//author: wuti
+//难度：简单
+//https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked
+
+
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+
+//这道题让我们求解在数组nums中两数之和等于target的下标
+//为了将数组中的元素本身与下标对应起来，我们使用unordered_map，可以认为是哈希表
+//unordered_map的key为数值本身，value为其在数组中的下标
+//对于搜索两个数字，我们使用target-nums[i]在map容器中寻找符合要求的元素
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {	
+		unordered_map<int,int> mp;
+		for (int i = 0; i < nums.size(); ++i) {
+			auto iter = mp.find(target - nums[i]);
+			mp.insert(make_pair(nums[i], i));
+			if (iter != mp.end()) {
+				return { iter->second,i };
+			}
+		}
+		return {};
+	}
+};
+
+int main() {
+	Solution so;
+	vector<int> nums{2, 7, 11, 15};
+	vector<int> ret=so.twoSum(nums, 9);
+	for (int i = 0; i < ret.size(); ++i) {
+		cout << ret[i] << " ";
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
